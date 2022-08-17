e8351b41b18788b95f506369e0e9bba68ecc2e0d
lobomfz <lobomfz@protonmail.com>
2022-08-17 14:42:37 -0300

    xorg aws test

--- archive/reboot.sh
+++ archive/reboot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pkill dolphin-emu
-PULSE_SERVER=host.docker.internal QT_QPA_PLATFORM=xcb dolphin-emu -v OGL -e ~/archive/${GAME_FILE}
+PULSE_SERVER=host.docker.internal dolphin-emu -v OGL -e ~/archive/${GAME_FILE}
